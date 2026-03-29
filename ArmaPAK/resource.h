6f1bbd5efa5263053c0d8d9f6e7ff52633b8d149
RoboGuru <114884227+Rendszerguru@users.noreply.github.com>
2026-03-29 16:55:53 +0200

    Update resource.h

--- ArmaPAK/resource.h
+++ ArmaPAK/resource.h
@@ -4,18 +4,19 @@
 
 #define IDD_ARMAPAK_SETTINGS            101
 #define IDD_ABOUTBOX                    102
+#define IDD_EXTRACT_OPTIONS             103
+
+#define IDB_LOGO                        104
 
 #define IDC_ENABLE_EDDS_CONVERSION      1001
 #define IDC_LOGO                        1002
-#define IDC_ABOUT_BUTTON 				1003
-#define IDC_ENABLE_LOG_INFO 			1004
+#define IDC_ABOUT_BUTTON                1003
+#define IDC_ENABLE_LOG_INFO             1004
+#define IDC_ENABLE_SMART_EXTRACT        1005
+#define IDC_KEEP_STRUCT                 1006
+#define IDC_SHOW_EXTRACT_PROMPT         1007
 
 #define IDC_ABOUT_LOGO                  1100
 #define IDC_ABOUT_VERSION_TEXT          1101
 #define IDC_ABOUT_AUTHOR_TEXT           1102
 #define IDC_ABOUT_SUPPORT_TEXT          1103
-
-#define IDB_LOGO                        103
-
-#define IDC_ENABLE_SMART_EXTRACT        1005
-#define IDC_KEEP_STRUCT                 1006
